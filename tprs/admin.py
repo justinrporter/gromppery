73a3058507cdc85499a7cb0d490fe4ac8f30a110
Justin R. Porter <justinrporter@gmail.com>
2017-04-23 09:41:25 -0500

    Add Alignment to admin page.

--- tprs/admin.py
+++ tprs/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Project, Submission
+from .models import Project, Submission, Alignment
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
@@ -8,4 +8,8 @@ class ProjectAdmin(admin.ModelAdmin):
 
 @admin.register(Submission)
 class SubmissionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Alignment)
+class AlignmentAdmin(admin.ModelAdmin):
     pass
