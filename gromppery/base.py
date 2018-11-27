28925184ab1e584784c93f60d3b20b6f751c4d90
Justin R. Porter <justinrporter@gmail.com>
2018-11-27 14:26:37 -0600

    bump django to 1.11, drf to 3.9

--- gromppery/base.py
+++ gromppery/base.py
@@ -76,7 +76,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
-    'PAGE_SIZE': 10
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 # Internationalization
